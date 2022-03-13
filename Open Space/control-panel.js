953bf14944c37a57b3142a925b166933eef7c3af
zhenya9876 <zhenya9809@mail.ru>
2022-03-14 00:45:45 +0500

    Added launch function and animation. Added launch button activetion after checking all inputs

--- Open Space/control-panel.js
+++ Open Space/control-panel.js
@@ -1,13 +1,19 @@
-let controlElements = [];
+let checkElements = [];
+let rangeElements = [];
 function disableControls(){
     for (let check of document.getElementsByClassName("check-buttons")[0]
         .getElementsByTagName("input")) {
-        controlElements.push(check);}
-    for (let lever of document.getElementsByClassName("lever")) {
-        controlElements.push(lever);
+        check.onchange = onControlChange;
+        checkElements.push(check);
     }
-    controlElements.push(document.getElementById("launchBtn"));
-    controlElements.forEach(value => value.disabled = true);
+    for (let lever of document.getElementsByClassName("lever")) {
+        lever.onchange = onControlChange;
+        rangeElements.push(lever);
+    }
+    document.getElementById("launchBtn").disabled = true;
+    document.getElementById("launchBtn").onclick = launch;
+    checkElements.forEach(value => value.disabled = true);
+    rangeElements.forEach(value => value.disabled = true);
     document.getElementById("okBtn").addEventListener("click", activatePanel);
 }
 window.addEventListener("load", disableControls);
@@ -15,6 +21,22 @@ function activatePanel() {
     document.getElementById("passBox").disabled = true;
     document.getElementById("okBtn").disabled = true;
    if (document.getElementById("passBox").value == "TrustNo1") {
-        controlElements.forEach(value => value.disabled = false);
+        rangeElements.forEach(value => value.disabled = false);
+        checkElements.forEach(value => value.disabled = false);
     }
+}
+function onControlChange() {
+    for (let checkElement of checkElements) {
+        if (!checkElement.checked) {
+            document.getElementById("launchBtn").disabled = true;
+            return false;}
+    }
+    for (let rangeElement of rangeElements) {
+        if (rangeElement.value!=100) {document.getElementById("launchBtn").disabled = true;
+            return false;}
+    }
+    document.getElementById("launchBtn").disabled = false;
+}
+function launch(){
+    document.getElementsByClassName("rocket")[0].classList.add("launched");
 }
